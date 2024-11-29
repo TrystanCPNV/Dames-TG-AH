bce6d2fc4061dfd94fdafda19e08fb4fd2476e60
AlexHuberCPNV <pv01sji@eduvaud.ch>
2024-11-29 08:54:02 +0100

    déplacement horizontal /limiter

--- Code perso.py
+++ Code perso.py
@@ -1,7 +1,8 @@
-
-#Nom        : Jeux_De_Dame
-#Author     : Alex_trystan
-#Date       : 28.11.2024
+"""
+    Nom        : Jeux_De_Dame
+    Author     : Alex_trystan
+    Date       : 28.11.2024
+"""
 
 import pygame
 
@@ -10,11 +11,11 @@ pygame.init()
 screen = pygame.display.set_mode((500, 500))
 screen.fill((255, 255, 255))
 
-# Load and scale the pion image
+
 pion = pygame.image.load("image\\MA-24_pion.png")
 pion = pygame.transform.scale(pion, (50, 50))
 
-# Draw the grid
+
 nb_colonne = 10
 nb_ligne = 10
 for i in range(nb_ligne + 1):
@@ -26,9 +27,9 @@ for i in range(nb_ligne + 1):
         else:
             pygame.draw.rect(screen, (255, 255, 255), (x_case, y_case, 50, 50))
 
-screen.blit(pion, (0, 0))  # Initial position of the pion
+screen.blit(pion, (0, 0))
 
-pion_position = [0, 0]
+pion_position = [4, 0]
 running = True
 while running:
     for event in pygame.event.get():
@@ -36,16 +37,25 @@ while running:
             running = False
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_RIGHT:
-                pion_position[0] = min(pion_position[0] + 1, nb_colonne - 1)
-                pion_position[1] = min(pion_position[1] + 1, nb_ligne - 1)
+                if pion_position[0] == 9:
+                    pion_position[0] = 9
+                elif pion_position[1] == 9:
+                    pion_position[0] = pion_position[0]
+                else:
+                    pion_position[0] = min(pion_position[0] + 1, nb_colonne - 1)
+                    pion_position[1] = min(pion_position[1] + 1, nb_ligne - 1)
             elif event.key == pygame.K_LEFT:
-                pion_position[0] = max(pion_position[0] - 1, 0,)
-                pion_position[1] = min(pion_position[1] + 1, nb_ligne - 1)
+                if pion_position[0] == 0:
+                    pion_position[0] = 0
+                elif pion_position[1] == 9:
+                    pion_position[0] = pion_position[0]
+                else:
+                    pion_position[0] = max(pion_position[0] - 1, 0,)
+                    pion_position[1] = min(pion_position[1] + 1, nb_ligne - 1)
 
-    screen.fill((255,255,255))
+    screen.fill((255, 255, 255))
 
-    # Refill the screen and redraw the grid
-    screen.fill((255, 255, 255))  # Clear the screen
+    screen.fill((255, 255, 255))
     for i in range(nb_ligne + 1):
         for y in range(nb_colonne + 1):
             x_case = i * 50
