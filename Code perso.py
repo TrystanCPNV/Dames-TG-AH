01fbe36cf9ae0e4c115294d22c96e0e7dfba283a
AlexHuberCPNV <pv01sji@eduvaud.ch>
2024-12-05 11:15:31 +0100

    vasi la c est bon ??

--- Code perso.py
+++ Code perso.py
@@ -25,18 +25,8 @@ def deplacer_pion_b_d():
         elif pion_position_blanc[1] == 9:
             pion_position_blanc[0] = pion_position_blanc[0]
         else:
-            pion_position_blanc[0] = max(pion_position_blanc[0] - 1, 0, )
+            pion_position_blanc[0] = max(pion_position_blanc[0] - 1, 0)
             pion_position_blanc[1] = min(pion_position_blanc[1] + 1, nb_ligne + 1)
-
-
-def deplacer_pion_h_g():
-    if pion_position_noir[0] == 0:
-        pion_position_noir[0] = 0
-    elif pion_position_noir[1] == 0:
-        pion_position_noir[0] = pion_position_noir[0]
-    else:
-        pion_position_noir[0] = max(pion_position_noir[0] + 1, 0, )
-        pion_position_noir[1] = min(pion_position_noir[1] - 1, nb_ligne - 1)
 
 
 def deplacer_pion_h_d():
@@ -45,7 +35,17 @@ def deplacer_pion_h_d():
     elif pion_position_noir[1] == 0:
         pion_position_noir[0] = pion_position_noir[0]
     else:
-        pion_position_noir[0] = max(pion_position_noir[0] - 1, 0, )
+        pion_position_noir[0] = min(pion_position_noir[0] + 1, nb_colonne - 1)
+        pion_position_noir[1] = min(pion_position_noir[1] - 1, nb_ligne - 1)
+
+
+def deplacer_pion_h_g():
+    if pion_position_noir[0] == 0:
+        pion_position_noir[0] = 0
+    elif pion_position_noir[1] == 10:
+        pion_position_noir[0] = pion_position_noir[0]
+    else:
+        pion_position_noir[0] = max(pion_position_noir[0] - 1, 0)
         pion_position_noir[1] = min(pion_position_noir[1] - 1, nb_ligne - 1)
 
 
@@ -56,7 +56,7 @@ screen.fill((255, 255, 255))
 pion_blanc = pygame.image.load("Image/MA-24_pion_blanc.png")
 pion_blanc = pygame.transform.scale(pion_blanc, (50, 50))
 
-pion_noir = pygame.image.load("Image/MA-24_pion_noir.png")
+pion_noir = pygame.image.load("Image/MA-24_pion_noir.jpg")
 pion_noir = pygame.transform.scale(pion_noir, (50, 50))
 
 
@@ -91,9 +91,6 @@ while running:
             if event.key == pygame.K_DOWN:
                 deplacer_pion_h_d()
 
-    screen.fill((255, 255, 255))
-
-    screen.fill((255, 255, 255))
     for i in range(nb_ligne + 1):
         for y in range(nb_colonne + 1):
             x_case = i * 50
@@ -103,10 +100,9 @@ while running:
             else:
                 pygame.draw.rect(screen, (255, 255, 255), (x_case, y_case, 50, 50))
 
-
-screen.blit(pion_blanc, (pion_position_blanc[0] * 50, pion_position_blanc[1] * 50))
-screen.blit(pion_noir, (pion_position_noir[0] * 50, pion_position_noir[1] * 50))
-pygame.display.flip()
+    screen.blit(pion_blanc, (pion_position_blanc[0] * 50, pion_position_blanc[1] * 50))
+    screen.blit(pion_noir, (pion_position_noir[0] * 50, pion_position_noir[1] * 50))
+    pygame.display.flip()
 
 pygame.quit()
 
